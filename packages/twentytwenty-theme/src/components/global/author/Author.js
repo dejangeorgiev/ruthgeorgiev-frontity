b9f1b0ffb251d774feb8de216133eab508132c1b
Dejan Georgiev <dejangeorgievv@gmail.com>
2020-08-20 00:54:59 +0200

    author additional info

--- packages/twentytwenty-theme/src/components/global/author/Author.js
+++ packages/twentytwenty-theme/src/components/global/author/Author.js
@@ -6,9 +6,9 @@ import FacebookIcon from "../icons/social-media/FacebookIcon";
 import InstagramIcon from "../icons/social-media/InstagramIcon";
 import PinterestIcon from "../icons/social-media/PinterestIcon";
 import YoutubeIcon from "../icons/social-media/YoutubeIcon";
+import LocationMarker from "../icons/png/LocationMarker.png"
 
 const Author = ({author, state, actions}) => {
-    console.log(author);
 
     /**
      * Once the post has loaded in the DOM, prefetch both the
@@ -21,43 +21,75 @@ const Author = ({author, state, actions}) => {
 
     return (
         <AuthorContainer>
-            <AuthorImage src={author.avatar_urls[96]} alt={author.name} />
+            <AuthorImage src={author.avatar_urls[96]} alt={author.name}/>
             <AboutContainer>
-                <AuthorName>{author.name}</AuthorName>
-                <AuthorTitle>{author.acf['userfieldgroup.title']}</AuthorTitle>
-                <AuthorDescription>{author.description}</AuthorDescription>
+                {author.name && (
+                    <AuthorName>{author.name}</AuthorName>
+                )}
+
+                {author.acf['userfieldgroup.title'] && (
+                    <AuthorTitle>{author.acf['userfieldgroup.title']}</AuthorTitle>
+                )}
+
+                {author.description && (
+                    <AuthorDescription>{author.description}</AuthorDescription>
+                )}
+
+                {author.acf['userfieldgroup.location'] && (
+                    <Location>
+                        <Marker src={LocationMarker}/>
+                        <LocationTitle>{author.acf['userfieldgroup.location']}</LocationTitle>
+                    </Location>
+                )}
             </AboutContainer>
-            <AuthorSocialMediaLink
-                href="https://www.facebook.com/ruthgeorgiev/"
-                target="_blank"
-                rel="noopener noreferrer"
-                social="facebook">
-                <FacebookIcon/>
-            </AuthorSocialMediaLink>
-            <AuthorSocialMediaLink
-                href="https://www.instagram.com/ruthgeorgiev/"
-                target="_blank"
-                rel="noopener noreferrer">
-                <InstagramIcon/>
-            </AuthorSocialMediaLink>
-            <AuthorSocialMediaLink
-                href="https://www.pinterest.ch/ruthgeorgiev/"
-                target="_blank"
-                rel="noopener noreferrer">
-                <PinterestIcon/>
-            </AuthorSocialMediaLink>
-            <AuthorSocialMediaLink
-                href="https://www.youtube.com/ruthgeorgiev/"
-                target="_blank"
-                rel="noopener noreferrer">
-                <YoutubeIcon/>
-            </AuthorSocialMediaLink>
-            <AuthorSocialMediaLink
-                href="https://twitter.com/GeorgievRuth"
-                target="_blank"
-                rel="noopener noreferrer">
-                <TwitterIcon/>
-            </AuthorSocialMediaLink>
+
+            {author.acf['userfieldgroup.facebook'] && (
+                <AuthorSocialMediaLink
+                    href={author.acf['userfieldgroup.facebook']}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    social="facebook">
+                    <FacebookIcon/>
+                </AuthorSocialMediaLink>
+            )}
+
+            {author.acf['userfieldgroup.instagram'] && (
+                <AuthorSocialMediaLink
+                    href={author.acf['userfieldgroup.instagram']}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <InstagramIcon/>
+                </AuthorSocialMediaLink>
+            )}
+
+
+            {author.acf['userfieldgroup.pinterest'] && (
+                <AuthorSocialMediaLink
+                    href={author.acf['userfieldgroup.pinterest']}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <PinterestIcon/>
+                </AuthorSocialMediaLink>
+            )}
+
+            {author.acf['userfieldgroup.youtube'] && (
+                <AuthorSocialMediaLink
+                    href={author.acf['userfieldgroup.youtube']}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <YoutubeIcon/>
+                </AuthorSocialMediaLink>
+            )}
+
+            {author.acf['userfieldgroup.twitter'] && (
+                <AuthorSocialMediaLink
+                    href={author.acf['userfieldgroup.twitter']}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <TwitterIcon/>
+                </AuthorSocialMediaLink>
+            )}
+
         </AuthorContainer>
     )
 };
@@ -71,15 +103,6 @@ const AuthorName = styled('h2')` ${tw`m-0 text-6xl`}`;
 const AuthorTitle = styled('div')` ${tw`text-3xl text-purple-500 pb-6`}`;
 const AuthorDescription = styled('div')` ${tw`font-body font-normal leading-tight tracking-normal text-3xl max-w-4xl mb-4`}`;
 const AuthorSocialMediaLink = styled('a')` ${tw`px-1`}`;
-
-/**
-<div className="md:flex bg-white rounded-lg p-6">
- <img className="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="avatar.jpg">
- <div className="text-center md:text-left">
- <h2 className="text-lg">Erin Lindford</h2>
- <div className="text-purple-500">Product Engineer</div>
- <div className="text-gray-600">erinlindford@example.com</div>
- <div className="text-gray-600">(555) 765-4321</div>
- </div>
- </div>
-**/
+const Location = styled('div')` ${tw``}`;
+const LocationTitle = styled('div')` ${tw`font-body font-normal text-4xl inline-block`}`;
+const Marker = styled('img')` ${tw`w-10 inline-block`}`;
